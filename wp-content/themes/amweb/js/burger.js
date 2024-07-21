1204f797093d7d43273d86f18c612157206d3cea
Marlene Audic <rondineau.m@gmail.com>
2024-07-21 15:56:28 +0200

    Responsive terminé

--- wp-content/themes/amweb/js/burger.js
+++ wp-content/themes/amweb/js/burger.js
@@ -4,14 +4,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const openBtn = document.getElementById('openBtn');
     const closeBtn = document.getElementById('closeBtn');
     const headerMenu = document.querySelector('.header_menu');
+    const menuLinks = headerMenu.querySelectorAll('#menu-header a');
 
+    // Ouvrir le menu
     openBtn.addEventListener('click', function(e) {
         e.preventDefault();
         headerMenu.classList.add('open');
     });
 
+    // Fermer le menu
     closeBtn.addEventListener('click', function(e) {
         e.preventDefault();
         headerMenu.classList.remove('open');
+    });
+
+    // Fermer le menu lorsque l'on clique sur un lien du menu
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            headerMenu.classList.remove('open');
+        });
+    });
+
+    // Assurez-vous que le menu se ferme si on clique en dehors de celui-ci
+    document.addEventListener('click', function(event) {
+        if (!headerMenu.contains(event.target) && !openBtn.contains(event.target) && !closeBtn.contains(event.target) && headerMenu.classList.contains('open')) {
+            headerMenu.classList.remove('open');
+        }
     });
 });
